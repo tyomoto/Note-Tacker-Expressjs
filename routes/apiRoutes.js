810ad97bb99906b07af87594598a66ec6e5fb434
T Omoto <tyler.omoto@gmail.com>
2022-06-20 01:30:42 -0400

    commented out delete function, and have functional code working to save notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,53 +1,62 @@
-const api = require('express').Router();
+const app = require('express').Router();
 const fs = require('fs');
-const database = require('../db/db.json');
+const path = require('path');
+let database = require('../db/db.json');
 
-api.get('./notes', (req, res) => {
+app.get('/notes', (req, res) => {
+    // .get is run to /notes, db.json file will be read at same time
     database = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
-
+  
     res.json(database);
-})
-
-api.post('/notes', (req, res) => {
+  
+  });
+  
+app.post('/notes', (req, res) => {
+    // seting up the structure for created note
     const {title, text} = req.body;
-    const newNote = {
-        title,
-        text,
-        id: Math.floor(Math.random() * 1000)
-    };
+    const newNote = { 
+    // Math.floor function to give the note a random ID
+      id: Math.floor(Math.random() * 1000),
+      title,
+      text
+    }
+  
+    // Push note into database
     database.push(newNote);
-    fs.writeFileSync('./db/db.json', 
-        JSON.stringify(database)(database), (err, res) => {
-            if(err) throw err;
+    // writes new note to db.json file
+    fs.writeFileSync('./db/db.json', JSON.stringify(database), (err, res) => {
+      if(err) throw err;
     });
- res.json(database);
-})
+    res.json(database);
+  })
+  
+//
 
-api.delete('/notes/:id'. (req, res) => {
-    const notesArrayKept = [];
+// app.delete('/notes/:id', (req, res) => {
+//     const notesArrayKept = [];
 
-    fs.readFile('./db.db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err); 
-        } else {
-            notesData = JSON.parse(data);
-            for (let i = 0; i < notesData.length; i++ ) {
-                let noteId = notesData[i].id;
-                if (noteId === req.parms.id){
-                    console.log(`This note has been deleted: ${noteId}`);
-                }else{
-                    notesArrayKept.push(notesData[i])
-                };
-            fs.writeFile('./db/db.json', JSON.stringify(notesArrayKept), (err, res) => {
-                if (err) throw err;
-            });
-            res.sendFile(path.join(__dirname, 'public/notes.html'));
-            };
-        };
+//     fs.readFile('./db.db.json', 'utf8', (err, data) => {
+//         if (err) {
+//             console.error(err); 
+//         } else {
+//             notesData = JSON.parse(data);
+//             for (let i = 0; i < notesData.length; i++ ) {
+//                 let noteId = notesData[i].id;
+//                 if (noteId === req.parms.id){
+//                     console.log(`This note has been deleted: ${noteId}`);
+//                 }else{
+//                     notesArrayKept.push(notesData[i])
+//                 };
+//             fs.writeFile('./db/db.json', JSON.stringify(notesArrayKept), (err, res) => {
+//                 if (err) throw err;
+//             });
+//             res.sendFile(path.join(__dirname, 'public/notes.html'));
+//             };
+//         };
 
         
-    });
+//     });
 
-});
+// });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const app = express();
 // creating environment variable port
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // ADD in ROUTES
 
