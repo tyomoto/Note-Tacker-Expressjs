f2dbc944ee76433a443e5f74854dc5e32d977caf
T Omoto <tyler.omoto@gmail.com>
2022-06-20 00:51:58 -0400

    added in api.delete script to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,3 +22,32 @@ api.post('/notes', (req, res) => {
     });
  res.json(database);
 })
+
+api.delete('/notes/:id'. (req, res) => {
+    const notesArrayKept = [];
+
+    fs.readFile('./db.db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err); 
+        } else {
+            notesData = JSON.parse(data);
+            for (let i = 0; i < notesData.length; i++ ) {
+                let noteId = notesData[i].id;
+                if (noteId === req.parms.id){
+                    console.log(`This note has been deleted: ${noteId}`);
+                }else{
+                    notesArrayKept.push(notesData[i])
+                };
+            fs.writeFile('./db/db.json', JSON.stringify(notesArrayKept), (err, res) => {
+                if (err) throw err;
+            });
+            res.sendFile(path.join(__dirname, 'public/notes.html'));
+            };
+        };
+
+        
+    });
+
+});
+
+module.exports = app;
